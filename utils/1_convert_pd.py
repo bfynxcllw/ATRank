3e059ff826bede2e8f3a1e95f91e3b3a8b54e651
bfynxcllw <48373275+bfynxcllw@users.noreply.github.com>
2019-11-18 23:06:11 +0800

    Update 1_convert_pd.py

--- utils/1_convert_pd.py
+++ utils/1_convert_pd.py
@@ -6,17 +6,17 @@ def to_df(file_path):
     df = {}
     i = 0
     for line in fin:
-      df[i] = eval(line)
+      df[i] = eval(line)#数值化
       i += 1
-    df = pd.DataFrame.from_dict(df, orient='index')
+    df = pd.DataFrame.from_dict(df, orient='index')#方向为行
     return df
 
 reviews_df = to_df('../raw_data/reviews_Electronics_5.json')
 with open('../raw_data/reviews.pkl', 'wb') as f:
-  pickle.dump(reviews_df, f, pickle.HIGHEST_PROTOCOL)
+  pickle.dump(reviews_df, f, pickle.HIGHEST_PROTOCOL)#将对象reviews_df保存到文件f中，HIGHEST_PROTOCOL意为最高配置
 
 meta_df = to_df('../raw_data/meta_Electronics.json')
-meta_df = meta_df[meta_df['asin'].isin(reviews_df['asin'].unique())]
-meta_df = meta_df.reset_index(drop=True)
+meta_df = meta_df[meta_df['asin'].isin(reviews_df['asin'].unique())]  #判断数列筛选，#asin为商品ID
+meta_df = meta_df.reset_index(drop=True)   #重置索引，多次写入会导致索引混乱
 with open('../raw_data/meta.pkl', 'wb') as f:
   pickle.dump(meta_df, f, pickle.HIGHEST_PROTOCOL)
